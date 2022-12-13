98d20e25cf436901f9543f9e2ea6a77b49fdeb0c
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-13 15:07:06 -0800

    fix publishing casts

--- src/helpers/publishCast.ts
+++ src/helpers/publishCast.ts
@@ -1,28 +1,10 @@
-import {
-  Farcaster,
-  FarcasterGuardianContentHost,
-} from '@standard-crypto/farcaster-js'
-import { Wallet } from 'ethers'
-import env from '@/helpers/env'
+import { MerkleAPIClient } from '@standard-crypto/farcaster-js'
 import wallet from '@/helpers/wallet'
 
-/**
- * Credit: https://gist.github.com/gskril/59d16fefbc411e61c9cce41963f3accf
- * MODIFIED FROM STANDARD-CRYPTO LIBRARY
- * Signs and publishes a simple text string.
- * The cast will be attributed to the username currently registered
- * to the given private key's address.
- */
-const _defaultFarcaster = new Farcaster()
-export default async function publishCast(text: string, replyTo: string) {
-  const contentHost = new FarcasterGuardianContentHost(wallet.privateKey)
-  const signer = new Wallet(wallet.privateKey)
-  const unsignedCast = await _defaultFarcaster.prepareCast({
-    fromUsername: env.USERNAME,
-    text,
-    replyTo,
-  })
-  const signedCast = await Farcaster.signCast(unsignedCast, signer)
-  await contentHost.publishCast(signedCast)
-  return signedCast
+const client = new MerkleAPIClient(wallet)
+
+export default async function (text: string, replyToId: string) {
+  const cast = await client.fetchCast(replyToId)
+  const publishedCast = await client.publishCast(text, cast)
+  return publishedCast.hash
 }
