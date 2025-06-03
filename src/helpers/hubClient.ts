7434b2d647f00108f8bf2103a04045317e793876
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-06-03 10:19:51 -0700

    fix: update HUB_URL to correct address

--- src/helpers/hubClient.ts
+++ src/helpers/hubClient.ts
@@ -1,8 +1,6 @@
-import {
-  getInsecureHubRpcClient,
-} from '@farcaster/hub-nodejs'
+import { getInsecureHubRpcClient } from '@farcaster/hub-nodejs'
 
-const HUB_URL = '34.172.154.21:2283'
+const HUB_URL = '34.172.154.21:3383'
 const hubClient = getInsecureHubRpcClient(HUB_URL)
 
 export default hubClient
